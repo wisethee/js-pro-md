d5bebcfa1412f86b7681ea93f1bbef6123606a70
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-01-20 18:26:34 +0000

    feat: update to es6 module import

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const fs = require("fs");
-const path = require("path");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+import fs from "fs";
+import inquirer from "inquirer";
+import path from "path";
+import generateMarkdown from "./utils/generate-markdown";
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "first_name",
+    message: "What's your first name",
+    default() {
+      return "John";
+    },
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
