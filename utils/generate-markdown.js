b1769fae7aef670689c3b879f29af1a655d4252a
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-01-22 22:56:19 +0000

    feat: add contributing to README.md, change badge position

--- utils/generate-markdown.js
+++ utils/generate-markdown.js
@@ -4,12 +4,16 @@ const generateMarkdown = (data) => {
 # :file_folder: ${data.title}
 ## Description
 ${data.description}
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](LICENSE)
 
-![Click to see the video(https://github.com/wisethee/js-pro-md/blob/feat/dev/assets/cover.jpg?raw=true)](https://github.com/wisethee/js-pro-md/blob/feat/dev/assets/screen-rec.mp4?raw=true)
+https://user-images.githubusercontent.com/31074930/213800562-2716d2e0-0ade-484e-bc23-63e2d27d8bca.mp4
+![Click to see the video](https://github.com/wisethee/js-pro-md/blob/feat/dev/assets/screen-rec.mp4?raw=true)
 
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
 * [Questions](#questions)
 * [License](#license)
 ## Installation
@@ -22,6 +26,8 @@ Run the following command to start the app.
 \`\`\`
 ${data.usage}
 \`\`\`
+## Contributing
+Contributions, issues and feature requests are welcome.  Browse the source repository of this article and open a pull request. I will do my best to review your proposal in due time.
 ## Tests
 The following command can be used to test the application.
 \`\`\`
@@ -31,8 +37,6 @@ ${data.tests}
 If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username}).
 ## License
 This project is licensed under the ${data.license} license.
-
-[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](LICENSE)
 `;
 };
 
